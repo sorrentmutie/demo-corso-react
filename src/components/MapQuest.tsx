5a13dc72d537ffbfc5bafeac417690862958e557
salvatore sorrentino <salvatore.sorrentino@live.com>
2023-11-29 13:27:35 +0100

    aggiornata mappa

--- src/components/MapQuest.tsx
+++ src/components/MapQuest.tsx
@@ -9,12 +9,11 @@ export const MapQuest: React.FC<MapQuestProps
   const key = "key=jSA1sVGnuPmhezWCJdiVfONPZntFFwQf";
   const url = `${urlBase}${key}&center=${city}&size=${width},${height}@2x`;
 
-const url2="https://www.mapquestapi.com/staticmap/v5/map?key=jSA1sVGnuPmhezWCJdiVfONPZntFFwQf&center=New+York&size=1100,500@2x";
 
   console.log(url);
   return (
     <div>
-      <img alt="" src={url2} {...rest} />
+      <img alt="" src={url} {...rest} />
     </div>
   );
 };
